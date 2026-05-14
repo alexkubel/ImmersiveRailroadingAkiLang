b74de6b66bbebc0101c5a36f899598b6debf4b23
Alexander Vicca <alexkub740558@gmail.com>
2026-05-14 13:49:53 +0100

    Update SimulationState.java

--- src/main/java/cam72cam/immersiverailroading/entity/physics/SimulationState.java
+++ src/main/java/cam72cam/immersiverailroading/entity/physics/SimulationState.java
@@ -494,10 +494,9 @@ public class SimulationState {
         // TODO This is kinda directional?
         double blockResistanceNewtons = interferingResistance * 1000 * Config.ConfigDamage.blockHardness;
 
-        //Gauge gauge = config.gauge;
-        //double yawDelta = DegreeFuncs.delta(config.stock.getFrontYaw(), config.stock.getRearYaw()) /
-        //        Math.abs(config.stock.getDefinition().getBogeyFront(gauge) - config.stock.getDefinition().getBogeyRear(gauge));
-        //
+        Gauge gauge = config.gauge;
+        double yawDelta = DegreeFuncs.delta(config.stock.getFrontYaw(), config.stock.getRearYaw()) /
+               Math.abs(config.stock.getDefinition().getBogeyFront(gauge) - config.stock.getDefinition().getBogeyRear(gauge));
         double curveResistanceNewtons = 0.0034 * (0.72 * gauge.value() + 0.47 * config.stock.getDefinition().getRigidWheelbase()) * yawDelta * defaultNewtons;
         
         double brakeCylinderNewtons = Math.max(config.designAdhesionNewtons * calculateBrakePressure(), config.handBrakeNewtons);
