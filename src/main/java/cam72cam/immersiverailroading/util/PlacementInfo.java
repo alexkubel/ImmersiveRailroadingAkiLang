147fdc0a2ae6ef68a4b9270dce12caa590c73659
Alexander Vicca <alexkub740558@gmail.com>
2026-08-15 10:06:36 +0100

    placement info fix?

--- src/main/java/cam72cam/immersiverailroading/util/PlacementInfo.java
+++ src/main/java/cam72cam/immersiverailroading/util/PlacementInfo.java
@@ -120,11 +120,11 @@ public class PlacementInfo {
 					}
 					break;
 			}
-		}
+	}
 		this.placementPosition = new Vec3d(new Vec3i(hit)).add(hitX, 0, hitZ);
 		this.direction = direction;
 		this.control = null;
-	}
+	
 
 	@Deprecated
 	public PlacementInfo(TagCompound nbt) {
